a00656c0a4c8491bd4e405270cf8104fd8a1afd8
kim-seonghyun <aruesin2@gmail.com>
2023-12-03 15:52:16 +0900

    feat: FrontServlet 구현. 모든 요청을 적절한 controller에 연결한 후, JSP 출력을 요청. 에러 처리를 위해 ERROR.jsp 작성

--- src/main/java/com/nhnacademy/shoppingmall/common/mvc/servlet/FrontServlet.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/mvc/servlet/FrontServlet.java
@@ -1,10 +1,18 @@
 package com.nhnacademy.shoppingmall.common.mvc.servlet;
 
+import static javax.servlet.RequestDispatcher.ERROR_EXCEPTION;
+import static javax.servlet.RequestDispatcher.ERROR_EXCEPTION_TYPE;
+import static javax.servlet.RequestDispatcher.ERROR_MESSAGE;
+import static javax.servlet.RequestDispatcher.ERROR_REQUEST_URI;
+import static javax.servlet.RequestDispatcher.ERROR_STATUS_CODE;
+
 import com.nhnacademy.shoppingmall.common.mvc.transaction.DbConnectionThreadLocal;
 import com.nhnacademy.shoppingmall.common.mvc.view.ViewResolver;
 import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
 import com.nhnacademy.shoppingmall.common.mvc.controller.ControllerFactory;
 
+import java.io.IOException;
+import java.sql.Connection;
 import lombok.extern.slf4j.Slf4j;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -22,10 +30,10 @@ public class FrontServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         //todo#7-1 controllerFactory를 초기화 합니다.
-
+        controllerFactory = new ControllerFactory();
 
         //todo#7-2 viewResolver를 초기화 합니다.
-
+        viewResolver = new ViewResolver();
     }
 
     // local variable 하기 때문에 Thread-safety하다.
@@ -33,16 +41,18 @@ public class FrontServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp){
         try{
             //todo#7-3 Connection pool로 부터 connection 할당 받습니다. connection은 Thread 내에서 공유됩니다.
+            DbConnectionThreadLocal.getConnection();
 
-
+            // controller 받아옴
             BaseController baseController = (BaseController) controllerFactory.getController(req);
+            // controller로 부터 JSP 경로 받아옴.
             String viewName = baseController.execute(req,resp);
 
             if(viewResolver.isRedirect(viewName)){
                 String redirectUrl = viewResolver.getRedirectUrl(viewName);
                 log.debug("redirectUrl:{}",redirectUrl);
                 //todo#7-6 redirect: 로 시작하면  해당 url로 redirect 합니다.
-
+                resp.sendRedirect(viewResolver.getRedirectUrl(redirectUrl));
             }else {
                 String layout = viewResolver.getLayOut(viewName);
                 log.debug("viewName:{}", viewResolver.getPath(viewName));
@@ -53,11 +63,26 @@ public class FrontServlet extends HttpServlet {
         }catch (Exception e){
             log.error("error:{}",e);
             DbConnectionThreadLocal.setSqlError(true);
+            req.setAttribute("status_code", req.getAttribute(ERROR_STATUS_CODE));
+            req.setAttribute("exception_type", req.getAttribute(ERROR_EXCEPTION_TYPE));
+            req.setAttribute("message", req.getAttribute(ERROR_MESSAGE));
+            req.setAttribute("exception", req.getAttribute(ERROR_EXCEPTION));
+            req.setAttribute("request_uri", req.getAttribute(ERROR_REQUEST_URI));
+            log.error("status_code:{}", req.getAttribute(ERROR_STATUS_CODE));
+            // Error jsp를 만들어야 돼?
+            RequestDispatcher rd = req.getRequestDispatcher(viewResolver.getPath("error"));
+            try {
+                rd.forward(req,resp);
+            } catch (ServletException ex) {
+                throw new RuntimeException(ex);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
             //todo#7-5 예외가 발생하면 해당 예외에 대해서 적절한 처리를 합니다.
 
         }finally {
             //todo#7-4 connection을 반납합니다.
-
+            DbConnectionThreadLocal.reset();
         }
     }
 
