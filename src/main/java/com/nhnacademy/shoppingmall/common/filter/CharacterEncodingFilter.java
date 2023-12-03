7ce9b3c7293569931d1d4a5ded489c44ebe0d64a
kim-seonghyun <aruesin2@gmail.com>
2023-12-03 16:28:56 +0900

    feat: CharacterEncodingFilter, encoding 방식을 결정한다.

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/CharacterEncodingFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/CharacterEncodingFilter.java
@@ -1,18 +1,32 @@
 package com.nhnacademy.shoppingmall.common.filter;
 
-import javax.servlet.*;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
-import java.io.IOException;
 
-public class CharacterEncodingFilter  implements Filter {
+@WebFilter(initParams = {@WebInitParam(name = "encoding", value = "UTF-8")})
+public class CharacterEncodingFilter implements Filter {
+    private String encoding;
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        //todo#8 UTF-8 인코딩, initParams의 encoding parameter value값을 charset 으로 지정합니다.
-        //@WebFilter(initParams = {@WebInitParam(name = "encoding",value = "UTF-8")})
-
-        filterChain.doFilter(servletRequest,servletResponse);
+    public void init(FilterConfig filterConfig) throws ServletException {
+        encoding = filterConfig.getInitParameter("encoding");
+        if (encoding == null) {
+            encoding = "UTF-8";
+        }
     }
 
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        //todo#8 UTF-8 인코딩, initParams의 encoding parameter value값을 charset 으로 지정합니다.
+        servletRequest.setCharacterEncoding(encoding);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
 }
