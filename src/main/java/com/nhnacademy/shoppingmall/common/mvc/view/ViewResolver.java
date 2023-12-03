7b388e96d727eba235f4b0355aa897db98b169e6
kim-seonghyun <aruesin2@gmail.com>
2023-12-03 15:05:19 +0900

    feature: ViewResolver 구현. Controller가 반환하는 View Name을 적절하게 처리하여 JSP 파일로 대응한다.

--- src/main/java/com/nhnacademy/shoppingmall/common/mvc/view/ViewResolver.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/mvc/view/ViewResolver.java
@@ -8,9 +8,13 @@ public class ViewResolver {
     public static final String DEFAULT_ADMIN_LAYOUT="/WEB-INF/views/layout/admin.jsp";
     public static final String LAYOUT_CONTENT_HOLDER = "layout_content_holder";
 
+    public static final String ADMIN_URL = "/admin/";
     private final String prefix;
     private final String postfix;
 
+    /**
+     * Controller가 반환하는 view Name을 WEB-INF/views 하위에 있는 JSP 파일로 대응하는 역할
+     */
     public ViewResolver(){
         this(DEFAULT_PREFIX,DEFAULT_POSTFIX);
     }
@@ -21,18 +25,25 @@ public class ViewResolver {
 
     public  String getPath(String viewName){
         //todo#6-1  postfix+viewNAme+postfix 반환 합니다.
-        return "";
+        if(viewName.startsWith("/")){
+            return prefix + viewName.substring(1) + postfix;
+        }else{
+            return prefix + viewName + postfix;
+
+        }
     }
 
     public boolean isRedirect(String viewName){
         //todo#6-2 REDIRECT_PREFIX가 포함되어 있는지 체크 합니다.
-        return false;
+        return viewName.toLowerCase().contains(REDIRECT_PREFIX);
     }
 
     public String getRedirectUrl(String viewName){
         //todo#6-3 REDIRECT_PREFIX를 제외한 url을 반환 합니다.
-
-        return "";
+        if (isRedirect(viewName.toLowerCase())) {
+            return viewName.substring(REDIRECT_PREFIX.length());
+        }
+        return viewName;
     }
 
     public String getLayOut(String viewName){
@@ -41,7 +52,10 @@ public class ViewResolver {
            /admin/경로가 포함되었다면 DEFAULT_ADMIN_LAYOUT 반환 합니다.
            /admin/경로가 포함되어 있지않다면 DEFAULT_SHOP_LAYOUT 반환 합니다.
         */
-
-        return DEFAULT_SHOP_LAYOUT;
+        if (viewName.contains(ADMIN_URL)) {
+            return DEFAULT_ADMIN_LAYOUT;
+        }else{
+            return DEFAULT_SHOP_LAYOUT;
+        }
     }
 }
