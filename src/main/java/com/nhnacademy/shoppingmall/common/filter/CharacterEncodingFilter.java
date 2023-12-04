f285353a9115d8f54413c8b2ae9a4e432e4622f7
kim-seonghyun <aruesin2@gmail.com>
2023-12-04 10:13:29 +0900

    fix: CharacterEncodingFilter, filter가 모든 url에 적용 되지 않고, 출력물 encoding 안되는 버그 해결

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/CharacterEncodingFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/CharacterEncodingFilter.java
@@ -9,8 +9,10 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
+import lombok.extern.slf4j.Slf4j;
 
-@WebFilter(initParams = {@WebInitParam(name = "encoding", value = "UTF-8")})
+@Slf4j
+@WebFilter(urlPatterns = "/*", initParams = {@WebInitParam(name = "encoding", value = "UTF-8")})
 public class CharacterEncodingFilter implements Filter {
     private String encoding;
 
@@ -26,7 +28,10 @@ public class CharacterEncodingFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
         //todo#8 UTF-8 인코딩, initParams의 encoding parameter value값을 charset 으로 지정합니다.
+        log.debug("encodingFilter 실행");
         servletRequest.setCharacterEncoding(encoding);
+        servletResponse.setCharacterEncoding(encoding);
         filterChain.doFilter(servletRequest, servletResponse);
+
     }
 }
