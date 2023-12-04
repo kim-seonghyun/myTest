ba8e443ecff4752f63512d16e53f0470d54e03d2
kim-seonghyun <aruesin2@gmail.com>
2023-12-04 10:11:21 +0900

    fix: FrontServlet, url과 Controller mapping이 안되는 에러 해결.

--- src/main/java/com/nhnacademy/shoppingmall/common/mvc/servlet/FrontServlet.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/mvc/servlet/FrontServlet.java
@@ -13,6 +13,7 @@ import com.nhnacademy.shoppingmall.common.mvc.controller.ControllerFactory;
 
 import java.io.IOException;
 import java.sql.Connection;
+import javax.servlet.ServletConfig;
 import lombok.extern.slf4j.Slf4j;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -28,9 +29,10 @@ public class FrontServlet extends HttpServlet {
     private ViewResolver viewResolver;
 
     @Override
-    public void init() throws ServletException {
+    public void init(ServletConfig config) throws ServletException {
         //todo#7-1 controllerFactory를 초기화 합니다.
-        controllerFactory = new ControllerFactory();
+        controllerFactory = (ControllerFactory) config.getServletContext()
+                .getAttribute(ControllerFactory.CONTEXT_CONTROLLER_FACTORY_NAME);
 
         //todo#7-2 viewResolver를 초기화 합니다.
         viewResolver = new ViewResolver();
@@ -39,10 +41,10 @@ public class FrontServlet extends HttpServlet {
     // local variable 하기 때문에 Thread-safety하다.
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp){
+        log.debug("service 실행됨");
+        DbConnectionThreadLocal.initialize();
         try{
             //todo#7-3 Connection pool로 부터 connection 할당 받습니다. connection은 Thread 내에서 공유됩니다.
-            DbConnectionThreadLocal.getConnection();
-
             // controller 받아옴
             BaseController baseController = (BaseController) controllerFactory.getController(req);
             // controller로 부터 JSP 경로 받아옴.
@@ -51,7 +53,7 @@ public class FrontServlet extends HttpServlet {
             if(viewResolver.isRedirect(viewName)){
                 String redirectUrl = viewResolver.getRedirectUrl(viewName);
                 log.debug("redirectUrl:{}",redirectUrl);
-                //todo#7-6 redirect: 로 시작하면  해당 url로 redirect 합니다.
+                //todo#7-6 redirect: 로 시작하면 해당 url로 redirect 합니다.
                 resp.sendRedirect(viewResolver.getRedirectUrl(redirectUrl));
             }else {
                 String layout = viewResolver.getLayOut(viewName);
@@ -82,6 +84,7 @@ public class FrontServlet extends HttpServlet {
 
         }finally {
             //todo#7-4 connection을 반납합니다.
+            log.debug("finally 실행됨" + req.getServletPath());
             DbConnectionThreadLocal.reset();
         }
     }
