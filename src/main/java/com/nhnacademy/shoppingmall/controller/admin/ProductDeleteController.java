c51fcbfdb0d3f0a5b6c72b999509124573ae9b5c
kim-seonghyun <aruesin2@gmail.com>
2023-12-06 22:11:47 +0900

    feature: 상품 삭제 기능 구현

---
+++ src/main/java/com/nhnacademy/shoppingmall/controller/admin/ProductDeleteController.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.shoppingmall.controller.admin;
+
+import com.nhnacademy.shoppingmall.Products.repository.ProductsRepository;
+import com.nhnacademy.shoppingmall.Products.repository.impl.ProductRepositoryImpl;
+import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
+import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping.Method;
+import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RequestMapping(method = Method.GET, value = "/admin/deleteProduct.do")
+public class ProductDeleteController implements BaseController {
+    ProductsRepository productsRepository = new ProductRepositoryImpl();
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) {
+        int productId = Integer.parseInt(req.getParameter("productId"));
+        productsRepository.deleteByProductId(productId);
+        return "redirect:/admin/productList.do";
+       
+    }
+}
